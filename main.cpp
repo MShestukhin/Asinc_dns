7455c9e16544e700486ded90f0d6a939baf9ed3a
MShestukhin <shist.95@mail.ru>
2018-03-01 20:44:58 -0500

    dns_naptr_0.1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include "dnsclient.h"
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    boost::asio::io_service io_service;
+    DnsClient* dns=new DnsClient(io_service);
+    char hostname[100];
+    int numberOfIteration=0;
+    printf("Enter hostname\n");
+    scanf("\n%s",hostname);
+    printf("\nEnter iteration number\n");
+    std::cin>>numberOfIteration;
+    dns->do_send(hostname,numberOfIteration);
+    io_service.run();
+    return 0;
+}
+
